481ad1c3a02b1c893677da1348ff87bedfc73e20
Paulo Ribeiro <paulo.ribeirodecarvalho@epfl.ch>
2024-04-26 00:09:22 +0200

    data attributes analysis functions

--- data_analysis.py
+++ data_analysis.py
@@ -47,3 +47,33 @@ def assets_information(data: pd.DataFrame):
           "\n%" + "%" * 38)
 
     return asset_info
+
+
+def attributes_type(data: pd.DataFrame):
+    """Separate attributes into numerical and categorical predictors."""
+    numerical_attributes = data.select_dtypes(include=['number']).columns
+    categorical_attributes = data.select_dtypes(exclude=['number']).columns
+    # Print the result of such quick analysis
+    print("%" * 11, "Attributes Type", "%" * 11,
+          f"\nNumber of numerical attributes: {len(numerical_attributes)}",
+          f"\nNumber of categorical attributes: {len(categorical_attributes)}",
+          "\n%" + "%" * 38)
+
+    return numerical_attributes, categorical_attributes
+
+
+def numerical_attributes_description(data: pd.DataFrame, column_name: list):
+    """Compute a brief statistical description of numerical attributes"""
+    # Initialize dictionaries to store information
+    numerical_info = {'attribute': [], 'min': [], 'max': [], 'mean': [], 'nan_ratio': []}
+    # For numerical attributes
+    for col in column_name:
+        nan_ratio = data[col].isnull().mean()
+        numerical_info['attribute'].append(col)
+        numerical_info['min'].append(data[col].min())
+        numerical_info['max'].append(data[col].max())
+        numerical_info['mean'].append(data[col].mean())
+        numerical_info['nan_ratio'].append(nan_ratio)
+    # Convert dict to pd.Dataframe
+    numerical_info_df = pd.DataFrame(numerical_info)
+    return numerical_info_df
