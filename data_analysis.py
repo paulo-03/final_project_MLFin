829375d51907779caefff92cf3aabc27682d902d
Paulo Debian <paulo.ribeirodecarvalho@epfl.ch>
2024-04-27 23:53:43 +0200

    adding metric evolution over time in data analysis

--- data_analysis.py
+++ data_analysis.py
@@ -3,6 +3,7 @@ Python script with useful functions for data analysis.
 """
 
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 
@@ -23,46 +24,52 @@ def asset_price_plot(data: pd.DataFrame, permno_id: int):
 
 def assets_information(data: pd.DataFrame):
     """Given the overall dataset, it returns the number of asset and some information about it."""
-    # Select only attributes that we are interested here
-    data = data[['permno', 'date', 'Price']]
     # Group by 'permno' and aggregate
     asset_info = data.groupby('permno').agg(
         data_quantity=('date', 'count'),
         date_from=('date', 'min'),
-        date_to=('date', 'max'),
-        ratio_price_nan=('Price', lambda x: x.isna().sum() / len(x))
+        date_to=('date', 'max')
     ).reset_index()
     # Calculate the date window
-    asset_info['date_window'] = (asset_info['date_to'] - asset_info['date_from'])
-    # Reorder the columns
-    asset_info = asset_info[['permno', 'data_quantity', 'date_from', 'date_to', 'date_window', 'ratio_price_nan']]
+    asset_info['month_window'] = (asset_info['date_to'].dt.year - asset_info['date_from'].dt.year) * 12 + (
+            asset_info['date_to'].dt.month - asset_info['date_from'].dt.month)
+    # Compute the ratio of Nan values in the data for each asset. Allows us to see if a specific asset has too many Nan
+    ratio_nan_attributes = data.groupby('permno').apply(lambda x: x.isnull().mean()).drop(columns=["date", "permno"])
+    asset_info['ratio_nan_price'] = ratio_nan_attributes['Price'].reset_index(drop=True)
+    asset_info['ratio_nan_overall'] = ratio_nan_attributes.mean(axis=1).reset_index(drop=True)
     # Print the number of different asset and the window time of our dataset
     asset_number = data['permno'].nunique()
     oldest_date = data['date'].min()
     latest_date = data['date'].max()
+
     print("%" * 10, "Asset Information", "%" * 10,
           f"\nNumber of assets: {asset_number}",
           f"\nOldest date in dataset: {oldest_date}",
           f"\nLatest date in dataset: {latest_date}",
           "\n%" + "%" * 38)
 
-    return asset_info
+    return asset_info, ratio_nan_attributes
 
 
-def attributes_type(data: pd.DataFrame):
-    """Separate attributes into numerical and categorical predictors."""
-    numerical_attributes = data.select_dtypes(include=['number']).columns
-    categorical_attributes = data.select_dtypes(exclude=['number']).columns
+def attributes_type(file_path: str):
+    """Separate attributes/predictors into continuous and discrete predictors."""
+    # Read the .csv file
+    data_description = pd.read_csv(filepath_or_buffer=file_path)
+    # Select only predictors to analyze them
+    data_description = data_description[data_description['Cat.Signal'] == 'Predictor']
+    # Separate continuous and discrete
+    continuous_attributes = data_description[data_description['Cat.Form'] == 'continuous']['Acronym']
+    discrete_attributes = data_description[data_description['Cat.Form'] == 'discrete']['Acronym']
     # Print the result of such quick analysis
-    print("%" * 11, "Attributes Type", "%" * 11,
-          f"\nNumber of numerical attributes: {len(numerical_attributes)}",
-          f"\nNumber of categorical attributes: {len(categorical_attributes)}",
-          "\n%" + "%" * 38)
+    print("%" * 10, "Attributes Type", "%" * 10,
+          f"\nNumber of continuous attributes: {len(continuous_attributes)}",
+          f"\nNumber of discrete attributes: {len(discrete_attributes)}",
+          "\n%" + "%" * 36)
 
-    return numerical_attributes, categorical_attributes
+    return continuous_attributes, discrete_attributes
 
 
-def numerical_attributes_description(data: pd.DataFrame, column_name: list):
+def continuous_attributes_description(data: pd.DataFrame, column_name: list):
     """Compute a brief statistical description of numerical attributes"""
     # Initialize dictionaries to store information
     numerical_info = {'attribute': [], 'min': [], 'max': [], 'mean': [], 'nan_ratio': []}
@@ -77,3 +84,31 @@ def numerical_attributes_description(data: pd.DataFrame, column_name: list):
     # Convert dict to pd.Dataframe
     numerical_info_df = pd.DataFrame(numerical_info)
     return numerical_info_df
+
+
+def evolution_over_time(data: pd.DataFrame):
+    """Compute few metrics evolution over time."""
+    #  Create a year column to ease the group by
+    data['year'] = data['date'].dt.year
+    # Group by 'year' and aggregate
+    data_evolution = data.groupby('year').agg(
+        asset_number=('permno', lambda x: x.nunique()),
+        data_quantity=('date', 'count'),
+    ).reset_index()
+    # Compute the ratio of Nan values in the data for each year. Allows us to see if a specific year has too many Nan
+    ratio_nan_attributes = data.groupby('year').apply(lambda x: x.isnull().mean()).drop(columns=["date", "permno"])
+    data_evolution['ratio_nan_price'] = ratio_nan_attributes['Price'].reset_index(drop=True)
+    data_evolution['ratio_nan_overall'] = ratio_nan_attributes.mean(axis=1).reset_index(drop=True)
+    # Finally plot all results to visualize them
+    fig, axs = plt.subplots(2, 2, figsize=(15, 10))
+
+    sns.lineplot(data=data_evolution, x='year', y='asset_number', ax=axs[0][0])
+    sns.lineplot(data=data_evolution, x='year', y='data_quantity', ax=axs[0][1])
+    sns.lineplot(data=data_evolution, x='year', y='ratio_nan_price', ax=axs[1][0])
+    sns.lineplot(data=data_evolution, x='year', y='ratio_nan_overall', ax=axs[1][1])
+    return data_evolution
+
+
+def _asset_number_over_time(data: pd.DataFrame):
+    """Compute the number of asset evolution over years"""
+    return data[['date', 'year']].groupby('year').count().reset_index()
--- helpers.py
+++ helpers.py
@@ -11,5 +11,7 @@ def load_data_df(file_path: str):
     # Convert their special date format to a more clear one
     data['yyyymm'] = pd.to_datetime(data['yyyymm'], format='%Y%m')
     data = data.rename(columns={'yyyymm': 'date'})  # rename the column
+    # Delete all rows with year 2024 because of lack of data
+    data = data[data['date'].dt.year != 2024]
 
     return data
