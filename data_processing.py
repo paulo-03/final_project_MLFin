99eb12e63774f3eb2dddcb7d413ea1570fa5f2d2
Paulo Ribeiro <paulo.ribeirodecarvalho@epfl.ch>
2024-05-09 16:46:35 +0200

    Reducing initial data because of compuational cost and also for data quality

--- data_processing.py
+++ data_processing.py
@@ -10,7 +10,7 @@ import pandas as pd
 from tqdm import tqdm_notebook as tqdm
 from helpers import load_data_df, select_time_window, data_per_month
 
-INFORMATION_ATTRIBUTE = ['permno', 'data', 'Price']
+INFORMATION_ATTRIBUTE = ['permno', 'date', 'Price']
 
 
 def impute_data(file_path: str, from_=None, to_=None, seed=42) -> pd.DataFrame:
@@ -34,10 +34,13 @@ def impute_data(file_path: str, from_=None, to_=None, seed=42) -> pd.DataFrame:
         # Cache the information attribute to not use them in the imputation
         data_info = data_month[INFORMATION_ATTRIBUTE]
         data_to_impute = data_month.drop(columns=INFORMATION_ATTRIBUTE)
+        char_str = data_to_impute.columns.tolist()
         # Perform the imputation
-        data_imputed.append(pd.DataFrame(imputer.fit_transform(data_to_impute)))
+        data_imputed_not_full = pd.DataFrame(data=imputer.fit_transform(data_to_impute), columns=char_str)
         # Concatenate the information attribute to the imputed data
-        data_imputed_full = pd.concat([data_info, data_imputed], axis=1).reset_index(drop=True)
+        data_imputed_full = pd.concat([data_info, data_imputed_not_full], axis=1).reset_index(drop=True)
+        # Store the fully imputed dataframe
+        data_imputed.append(data_imputed_full)
 
     # Finally, concatenate all imputed data to reform the initial dataframe
     print("Performing the concatenation...")
--- helpers.py
+++ helpers.py
@@ -11,8 +11,9 @@ def load_data_df(file_path: str) -> pd.DataFrame:
     # Read the .csv file
     data = pd.read_csv(filepath_or_buffer=file_path)
     # Convert their special date format to a more clear one
-    data['yyyymm'] = pd.to_datetime(data['yyyymm'], format='%Y%m')
-    data = data.rename(columns={'yyyymm': 'date'})  # rename the column
+    if 'date' not in data.columns.tolist():  # If already formatted, pass this
+        data['yyyymm'] = pd.to_datetime(data['yyyymm'], format='%Y%m')
+        data = data.rename(columns={'yyyymm': 'date'})  # rename the column
     # Delete all rows with year 2024 because of lack of data
     data = data[data['date'].dt.year != 2024]
 
