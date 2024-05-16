b2398c3754960cd29e444ac30158c8cc65f1a138
RamiATASSI <114172598+RamiATASSI@users.noreply.github.com>
2024-05-16 15:19:17 +0200

    pearson_correlation.py and random_forest.py

--- models/pearson_correlation.py
+++ models/pearson_correlation.py
@@ -1,3 +1,66 @@
 """
 This python script propose a class containing all useful method in a pearson correlation.
 """
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
+class PearsonCorrelation:
+    def __init__(self, data: pd.DataFrame):
+        self.correlation_matrix = data.drop(columns=['permno', 'date']).corr()
+
+    def get_sorted_correlation_pairs(self, ascending=False, top_k_pairs: int = None) -> pd.DataFrame:
+        # Reset the index
+        correlation_matrix_reset = self.correlation_matrix.reset_index()
+
+        # Melt the DataFrame
+        melted = correlation_matrix_reset.melt(id_vars='index')
+
+        # Create a new column that contains the pair of columns
+        melted['pair'] = list(zip(melted['index'], melted['variable']))
+
+        # Set the new column as the index and select the value column
+        flattened = melted.set_index('pair')['value'].rename('correlation')
+
+        # Print the flattened Series
+        correlation_pairs = flattened.sort_values(ascending=ascending)[flattened != 1].iloc[::2].reset_index()
+        # If k_first is provided, return only the first k_first elements
+        if top_k_pairs is not None:
+            return correlation_pairs.head(top_k_pairs)
+
+        return correlation_pairs
+
+    def _plot_mean_correlation(self):
+        means = self.correlation_matrix.mean().sort_values()
+        # plot means
+        plt.figure(figsize=(20, 10))
+        sns.barplot(x=means.index, y=means.values)
+        plt.xticks(rotation=90)  # Rotate x-axis labels by 90 degrees
+        plt.title('Mean Correlation')
+        plt.show()
+
+    def _plot_median_correlation(self):
+        medians = self.correlation_matrix.median().sort_values()
+        # plot medians
+        plt.figure(figsize=(20, 10))
+        sns.barplot(x=medians.index, y=medians.values)
+        plt.xticks(rotation=90)  # Rotate x-axis labels by 90 degrees
+        plt.title('Median Correlation')
+        plt.show()
+
+    def _plot_correlation_matrix_heatmap(self):
+        plt.figure(figsize=(20, 20))
+        sns.heatmap(self.correlation_matrix, annot=True, cmap='Reds', fmt=".2f", linewidths=0.5)
+        plt.title('Correlation')
+        plt.show()
+
+    def plot(self, plot_type: str = 'heatmap'):
+        if plot_type == 'heatmap':
+            self._plot_correlation_matrix_heatmap()
+        elif plot_type == 'mean':
+            self._plot_mean_correlation()
+        elif plot_type == 'median':
+            self._plot_median_correlation()
+        else:
+            raise ValueError('Plot type not supported')
--- models/random_forest.py
+++ models/random_forest.py
@@ -1,3 +1,90 @@
 """
 This python script propose a class containing all useful method in a random forest.
 """
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.model_selection import cross_val_score, GridSearchCV, KFold, train_test_split
+from sklearn.tree import plot_tree
+
+
+class RandomForest:
+    def __init__(self, data: pd.DataFrame, target: str = 'return'):
+        self.target = target
+
+        data['date'] = pd.to_datetime(data['date'])
+        self.X = data.drop(columns=['return', 'log_return', 'log_price', 'date'])
+        self.y = data[self.target]
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.2,
+                                                                                random_state=0, shuffle=True)
+
+        self.data_shape = data.shape
+        self.n_estimators = 50
+        self.max_depth = 3
+        self.max_features = 1.0
+        self.model = RandomForestRegressor(n_estimators=self.n_estimators, max_depth=self.max_depth,
+                                           max_features=self.max_features, n_jobs=-1, random_state=0)
+
+    def hyperparameter_tuning_with_crossvalidation(self, n_estimators: list = None, max_depth: list = None,
+                                                   max_features: list = None, cv_splits: int = 5):
+        if n_estimators is None:
+            n_estimators = [50, 300]
+        if max_depth is None:
+            max_depth = [None, 3]
+        if max_features is None:
+            max_features = [1.0, 'sqrt']
+
+        param_grid = {
+            'n_estimators': n_estimators,
+            'max_depth': max_depth,
+            'max_features': max_features
+        }
+        kf = KFold(n_splits=cv_splits, shuffle=True, random_state=0)
+        grid_search = GridSearchCV(self.model, param_grid, cv=kf, n_jobs=-1, scoring='r2')
+        grid_search.fit(self.X, self.y)
+        return grid_search.best_params_
+
+    def set_model(self, n_estimators: list = 50, max_depth: list = 3, max_features: list = 1.0):
+        self.n_estimators = n_estimators
+        self.max_depth = max_depth
+        self.max_features = max_features
+        self.model = RandomForestRegressor(n_estimators=n_estimators, max_depth=max_depth, max_features=max_features,
+                                           n_jobs=-1, random_state=0)
+
+    def fit_predict_and_print_score(self):
+        self.model.fit(self.X_train, self.y_train)
+        prediction = self.model.predict(self.X_test)
+        print(f'With the following data:\n'
+              f'- Data shape: {self.data_shape}\n'
+              f'- Target: {self.target}\n'
+              f'and the following hyperparameters:\n'
+              f'- n_estimators: {self.n_estimators}\n'
+              f'- max_depth: {self.max_depth}\n'
+              f'- max_features: {self.max_features}')
+        print('The R2 score is:', r2_score(self.y_test, prediction))
+        print('The MSE is:', mean_squared_error(self.y_test, prediction))
+
+    def plot_feature_importance(self):
+        feature_importance = pd.Series(self.model.feature_importances_, index=self.X.columns).sort_values(
+            ascending=False)
+        fe
+        plt.figure(figsize=(20, 10))
+        sns.barplot(x=feature_importance.index, y=feature_importance.values)
+        plt.xticks(rotation=90)  # Rotate x-axis labels by 90 degrees
+        plt.title('Median Correlation')
+        plt.show()
+
+    def plot_decision_tree(self, num_trees_to_plot: int = 3):
+        for tree_in_forest in range(num_trees_to_plot):
+            plt.figure(figsize=(20, 10))
+            plot_tree(self.model.estimators_[tree_in_forest],
+                      filled=True,
+                      rounded=True,
+                      feature_names=self.X.columns.tolist())
+            plt.title(f'Tree number: {tree_in_forest + 1}')
+            plt.show()
+
+    def cross_val_score(self, cv: int = 5, scoring: str = 'r2'):
+        return cross_val_score(self.model, self.X, self.y, cv=cv, n_jobs=-1, scoring=scoring)
